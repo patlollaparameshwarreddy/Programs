02e44e80e7837de34bb8d335dfdb207fc326297e
Parameshwar Reddy Patlolla <patlollaparameshwarreddy7@gmail.com>
2019-01-18 17:35:35 +0530

    first commit

---
+++ Utility.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Utility.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Fellowship
+{
+    using System;
+
+    /// <summary>
+    /// this class is used for declaring repeated task
+    /// </summary>
+    public class Utility
+    {
+        /// <summary>
+        /// Gets the integer will return the integer value
+        /// </summary>
+        /// <returns>integer required by the programmer</returns>
+        public static int GetInt()
+        {
+            ////converting string to integer
+            int integer = Convert.ToInt32(Console.ReadLine());
+            return integer;
+        }
+
+        /// <summary>
+        /// get double will return the double value
+        /// </summary>
+        /// <returns>returns double value required by the programmer</returns>
+        public static double GetDouble()
+        {
+            ////string id converted to double
+            double d = Convert.ToDouble(Console.ReadLine());
+            return d;
+        }
+    }
+}
